2c57dfa8934d3ee9a6220474bba0fd025a6bc5e7
michaelcurran <curran.mike@gmail.com>
2016-06-28 10:35:36 -0700

    Update styling

--- lib/dbb.rb
+++ lib/dbb.rb
@@ -1,3 +1,4 @@
+# Used for communicating with AWS DynamoDB
 class DdbKms < Kms
   def initialize(table)
     @table = table
@@ -5,81 +6,80 @@ class DdbKms < Kms
     super()
   end
 
-  def putdb(key,value)
-    #Makes sure there's a value to put so it isn't null
+  def putdb(key, value)
+    # Makes sure there's a value to put so it isn't null
     if value.nil?
-      abort("ABORTED! Did you enter in a value to encrypt?")
+      abort('ABORTED! Did you enter in a value to encrypt?')
       exit 1
     end
 
-    #Store the value into a temp file
+    # Store the value into a temp file
     file = '.tmp-encryptdb-_-1357915'
     File.open(file, 'wb') do |f|
       f.puts value
     end
 
-    #Retrieve the encrypted data
+    # Retrieve the encrypted data
     encrypted = encrypt(file)
 
-    #Parse the JSON
+    # Parse the JSON
     data = JSON.parse(encrypted)
 
-    #Cleanup the temp file
+    # Cleanup the temp file
     File.delete(file)
 
-    #Store the key/value
-    @ddb.put_item({
+    # Store the key/value
+    @ddb.put_item(
       table_name: @table,
       item: {
-        "name" => "#{key}",
-        "ciphertext" => "#{data['ciphertext']}",
-        "dataKey" => "#{data['dataKey']}",
-        "iv" => "#{data['iv']}",
-        "salt" => "#{data['salt']}"
-      }})
+        'name' => key.to_s,
+        'ciphertext' => data['ciphertext'],
+        'data_key' => data['data_key'],
+        'iv' => data['iv'],
+        'salt' => data['salt']
+      })
   end
 
   def getdb(key)
-    #Get the encrypted data for a key
-    resp = @ddb.get_item({table_name: @table, key: { "name" => "#{key}"}})
+    # Get the encrypted data for a key
+    resp = @ddb.get_item(table_name: @table, key: { 'name' => key })
     resp.item.to_json
   end
 
   def decryptdb(key)
-    #Gets the encrypted data for the key
-    resp = @ddb.get_item(table_name: @table, key: { "name" => "#{key}"})
+    # Gets the encrypted data for the key
+    resp = @ddb.get_item(table_name: @table, key: { 'name' => key })
 
-    #Stores it in a tmp file as json
+    # Stores it in a tmp file as json
     file = '.tmp-encryptdb-_-1357915'
 
-    #Use shared memory if available
-    if File.directory?('/dev/shm')
-      fullFile = "/dev/shm/#{file}"
-    else
-      fullFile = Dir.pwd + "/#{file}"
-    end
+    # Use shared memory if available
+    full_file = if File.directory?('/dev/shm')
+                  "/dev/shm/#{file}"
+                else
+                  Dir.pwd + "/#{file}"
+                end
 
-    File.open(fullFile, 'wb') do |f|
+    File.open(full_file, 'wb') do |f|
       f.puts resp.item.to_json
     end
 
-    puts decrypt(fullFile)
+    puts decrypt(full_file)
 
-    #Cleanup
-    File.delete(fullFile)
+    # Cleanup
+    File.delete(full_file)
   end
 
   def listdb
-    #lists all the key names
-    resp = @ddb.scan({table_name: @table, attributes_to_get: ["name"]})
+    # Lists all the key names
+    resp = @ddb.scan(table_name: @table, attributes_to_get: ['name'])
     resp.count.times do |i|
-      puts resp.items[i]["name"]
+      puts resp.items[i]['name']
     end
   end
 
   def deletedb(key)
-    #deletes a key from the table
-    @ddb.delete_item({table_name: @table, key: { "name" => "#{key}"}})
+    # Deletes a key from the table
+    @ddb.delete_item(table_name: @table, key: { 'name' => key })
   end
-
 end
--- lib/s3.rb
+++ lib/s3.rb
@@ -1,3 +1,4 @@
+# Used for communicating with AWS
 class S3Kms < Kms
   def initialize(bucket)
     @bucket = bucket
@@ -6,68 +7,75 @@ class S3Kms < Kms
   end
 
   def puts3(file)
-    #Parse out only the file name (useful due to the updates3 method)
-    fileName = file.split("/")[-1]
-    fileName = fileName.split(".updated-_-1357915")[0]
+    # Parse out only the file name (useful due to the updates3 method)
+    file_name = file.split('/')[-1]
+    file_name = file_name.split('.updated-_-1357915')[0]
 
     File.open(file, 'rb') do |f|
-      @s3.put_object(bucket: @bucket, key: fileName, body: f)
+      @s3.put_object(bucket: @bucket, key: file_name, body: f)
     end
   end
 
   def gets3(file)
     File.open(file, 'wb') do |f|
-      obj = @s3.get_object({ bucket: @bucket, key: file }, target: f)
+      @s3.get_object({ bucket: @bucket, key: file }, target: f)
     end
   end
 
   def updates3(file)
-    #Use shared memory if available
-    if File.directory?('/dev/shm')
-      fullFile = "/dev/shm/#{file}"
-    else
-      fullFile = Dir.pwd + "/#{file}"
-    end
+    # Use shared memory if available
+    full_file = if File.directory?('/dev/shm')
+                  "/dev/shm/#{file}"
+                else
+                  Dir.pwd + "/#{file}"
+                end
 
-    tmp = "#{fullFile}.tmp-_-1357915"
-    decrypted = "#{fullFile}.decrypted-_-1357915"
-    updated = "#{fullFile}.updated-_-1357915"
+    tmp = "#{full_file}.tmp-_-1357915"
+    decrypted = "#{full_file}.decrypted-_-1357915"
+    updated = "#{full_file}.updated-_-1357915"
 
-    #Grab the file and store it as a tmp file
+    # Grab the file and store it as a tmp file
     File.open(tmp, 'wb') do |f|
-      obj = @s3.get_object({ bucket: @bucket, key: file }, target: f)
+      @s3.get_object({ bucket: @bucket, key: file }, target: f)
     end
 
-    #Decrypt to a file and edit
+    # Decrypt to a file and edit
     File.open(decrypted, 'wb') do |f|
       f.puts decrypt(tmp)
     end
 
     system('vi', decrypted)
 
-    #Encrypt the updated file and upload it
+    # Encrypt the updated file and upload it
     File.open(updated, 'wb') do |f|
       f.puts encrypt(decrypted)
     end
 
     puts3(updated)
 
-    #Cleanup
+    # Cleanup
     File.delete(decrypted, updated, tmp)
   end
 
   def deletes3(file)
-    #Delete the file
+    # Delete the file
     @s3.delete_object(bucket: @bucket, key: file)
   end
 
   def lists3
     resp = @s3.list_objects(bucket: @bucket)
+    contents = resp.contents
 
-    #List out the file names, sizes, and dates
-    printf "%-40s\t%s\t%s\n", "Name", "Size", "Last Modified"
-    (0..resp.contents.length-1).each do |num|
-      printf "%-40s\t%s\t%s\n", resp.contents[num].key, resp.contents[num].size, resp.contents[num].last_modified
+    # List out the file names, sizes, and dates
+    printf "%-40s\t%s\t%s\n", 'Name', 'Size', 'Last Modified'
+
+    (0..resp.contents.length - 1).each do |num|
+      printf(
+        "%-40s\t%s\t%s\n",
+        contents[num].key,
+        contents[num].size,
+        contents[num].last_modified
+      )
     end
   end
 end
